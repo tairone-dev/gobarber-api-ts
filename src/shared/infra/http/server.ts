35127829df1da84d154dce81e438597e43728cba
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2020-08-19 07:49:19 -0300

    Refactored project architecture with DDD

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,11 +4,11 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
-import routes from './routes';
-import uploadConfig from './config/upload';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 
-import './database';
-import AppError from './errors/AppError';
+import '@shared/infra/typeorm';
+import routes from './routes';
 
 const app = express();
 
