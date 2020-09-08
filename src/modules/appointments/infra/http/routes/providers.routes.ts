2979d42a3c60e8885b7c55ce92029d79744fbca1
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2020-09-08 12:08:50 -0300

    Added availability controller and routes

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,13 +1,25 @@
 import { Router } from 'express';
 
-import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController';
+import ProviderMonthAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderDayAvailabilityController';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+providersRouter.get(
+  '/:provider_id/availabilities/months',
+  providerMonthAvailabilityController.index,
+);
+providersRouter.get(
+  '/:provider_id/availabilities/days',
+  providerDayAvailabilityController.index,
+);
 
 export default providersRouter;
