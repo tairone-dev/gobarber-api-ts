b28f686311884e5b4046d968ae7c539073da9b5b
Tairone Livinalli <tairone.livinalli@softplan.com.br>
2020-09-08 21:29:55 -0300

    Added class transformer

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import CreateSessionService from '@modules/users/services/CreateSessionService';
 
@@ -11,6 +12,6 @@ export default class SessionsController {
 
     const { user, token } = await createSession.execute({ email, password });
 
-    return response.json({ user, token });
+    return response.json({ user: classToClass(user), token });
   }
 }
